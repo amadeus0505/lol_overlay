54a6a366e5f635023c8e2d6179ae7a5e8b30193a
amadeus0505 <42494998+amadeus0505@users.noreply.github.com>
2022-07-02 00:14:45 +0200

    adding controls

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ class MainWindow(QMainWindow):
 
         self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint | Qt.Tool)
         self.setAttribute(Qt.WA_TranslucentBackground)
-        self.setWindowOpacity(0.5)
+        self.setWindowOpacity(self.transparancy_low)
         self.not_clickable = True
         self.setWindowFlag(Qt.WindowTransparentForInput, self.not_clickable)
 
@@ -45,11 +45,17 @@ class MainWindow(QMainWindow):
 
         self.main_layout = QHBoxLayout()
 
-        self.label = QLabel("")
-        self.label.setMinimumHeight(20)
-        self.label.setMaximumWidth(int(self.WIDTH/3*2))
-        self.label.setAlignment(Qt.AlignTop)
-        self.main_layout.addWidget(self.label)
+        self.lbl_songinfo = QLabel("")
+        self.lbl_songinfo.setMinimumHeight(15)
+        self.lbl_songinfo.setMaximumWidth(int(self.WIDTH / 3 * 2))
+        self.lbl_songinfo.setAlignment(Qt.AlignTop)
+        self.main_layout.addWidget(self.lbl_songinfo)
+
+        self.btn_pause = QPushButton("⏸")
+        self.btn_pause.setMinimumHeight(20)
+        self.btn_pause.setStyleSheet("border: 1px solid;")
+        self.main_layout.setContentsMargins(0)
+        self.main_layout.addWidget(self.btn_pause)
 
         self.btn_quit = QPushButton("Quit")
         self.btn_quit.clicked.connect(exit)
@@ -74,6 +80,7 @@ class MainWindow(QMainWindow):
             opacity += 0.1
             self.setWindowOpacity(opacity)
             time.sleep(0.001)
+        super().enterEvent(a0)
 
     def leaveEvent(self, a0: QEvent) -> None:
         opacity = self.transparancy_high
@@ -81,19 +88,19 @@ class MainWindow(QMainWindow):
             opacity -= 0.1
             self.setWindowOpacity(opacity)
             time.sleep(0.001)
+        super().leaveEvent(a0)
 
     def keyPress(self, key):
         try:
             if key.char == "ß":
-                print(self.not_clickable)
                 self.not_clickable = not self.not_clickable
+                print(self.not_clickable)
                 self.setWindowFlag(Qt.WindowTransparentForInput, self.not_clickable)
+                print("before")
+                self.show()
+                print("showed")
         except AttributeError:
             pass
-
-        print("before")
-        self.show()
-        print("showed")
 
     def closeEvent(self, event: QCloseEvent) -> None:
         self.keyboard_listener.stop()
@@ -101,7 +108,7 @@ class MainWindow(QMainWindow):
 
     def update_label(self):
         name, artists = handle_spotify.get_current_track()
-        self.label.setText(f"{name} - {', '.join(artists)}")
+        self.lbl_songinfo.setText(f"{name} - {', '.join(artists)}")
 
     # mouse press, move and releas event to make window draggable
     def mousePressEvent(self, event):
